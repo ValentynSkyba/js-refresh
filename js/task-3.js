cf400a6499ee642e7bf6d6b5d90b63de1d191ea1
ValentynSkyba <valentynskyba@gmail.com>
2024-05-22 23:34:48 +0100

    Done!

--- js/task-3.js
+++ js/task-3.js
@@ -1,14 +1,26 @@
-"use strict"
+'use strict';
 
-function getElementWidth (content, padding, border) {
-    return parseFloat(content) + parseFloat(padding)*2 + parseFloat(border)*2;
-    }
+// Напиши функцію filterArray(numbers, value), яка приймає масив чисел (numbers) та значення (value) як параметри. Функція повинна повертати новий масив лише тих чисел із масиву numbers, які більші за значення value.
 
-//   content = parseFloat(content);
-//   padding = parseFloat(padding);
-//   border = parseFloat(border);
-//   return content + 2 * padding + 2 * border;
+// Усередині функції:
 
-console.log(getElementWidth("50px", "8px", "4px")); // 74
-console.log(getElementWidth("60px", "12px", "8.5px")); // 101
-console.log(getElementWidth("200px", "0px", "0px")); // 200
+// Створи порожній масив, у який будеш додавати підходящі числа.
+// Використай цикл для ітерації кожного елемента масиву numbers.
+// Використовуй умовний оператор if усередині циклу для перевірки кожного елемента и додавання до свого масиву.
+// Поверни свій новий масив з підходящими числами як результат.
+
+// Візьми код нижче і встав після оголошення своєї функції для перевірки коректності її роботи. У консоль будуть виведені результати її роботи.
+
+function filterArray(numbers, value) {
+  const newArray = [];
+  for (const number of numbers) {
+    if (number > value) newArray.push(number);
+  }
+  return newArray;
+}
+
+console.log(filterArray([1, 2, 3, 4, 5], 3)); // [4, 5]
+console.log(filterArray([1, 2, 3, 4, 5], 4)); // [5]
+console.log(filterArray([1, 2, 3, 4, 5], 5)); // []
+console.log(filterArray([12, 24, 8, 41, 76], 38)); // [41, 76]
+console.log(filterArray([12, 24, 8, 41, 76], 20)); // [24, 41, 76]
